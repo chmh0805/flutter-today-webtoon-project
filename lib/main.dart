7579e6f1ba445cc28d903d35b0a37dea239a5794
chmh0805 <chmh0805@gmail.com>
2023-01-26 23:38:51 +0900

    Feat: Put data in state after fetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:today_webtoon/screens/home_screen.dart';
-import 'package:today_webtoon/services/api_service.dart';
 
 void main() {
-  ApiService().getTodaysToons();
   runApp(const App());
 }
 
